015f12d9fdf1fbff02fe3261c3ab662f2a068a0a
路赛 <sai.lu@itdev.xiangguang.com>
2018-10-26 17:54:36 +0800

    Virtual Scroll CDK

---
+++ src/app/basic-scroll/basic-scroll.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { emojiRandom } from '../emojis';
+// import * as faker from 'faker/locale/zh_CN';
+import * as faker from 'faker';
+
+@Component({
+  selector: 'app-basic-scroll',
+  templateUrl: './basic-scroll.component.html',
+  styleUrls: ['./basic-scroll.component.scss']
+})
+export class BasicScrollComponent {
+  people;
+
+  constructor() {
+    this.people = Array(100)
+      .fill(1)
+      .map(_ => {
+        return {
+          name: faker.name.findName(),
+          bio: faker.hacker.phrase(),
+          emoji: emojiRandom()
+        };
+      });
+  }
+}
